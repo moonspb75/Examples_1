59e55bdb0dc4655738ad40f5de85d97b351055db
Yanina <moonspb75@mail.ru>
2023-02-11 21:50:50 +0300

    ДЗ

--- task_2switch/Program.cs
+++ task_2switch/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+switch (day)
+{
+    case 1:
+        Console.Write("Понедельник");
+        break;
+
+    case 2:
+        Console.Write("Вторник");
+        break;
+
+    case 3:
+        Console.Write("Среда");
+        break;
+
+    case 4:
+        Console.Write("Четверг");
+        break;
+
+    case 5:
+        Console.Write("Пятница");
+        break;
+
+    case 6:
+        Console.Write("Суббота");
+        break;
+
+    case 7:
+        Console.Write("Воскресенье");
+        break;
+
+    default:
+        Console.Write("Не верное значение!");
+        break;
+}
